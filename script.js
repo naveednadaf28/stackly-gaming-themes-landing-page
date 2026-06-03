68e81017be3ca4cc5f67fb56fb6078744b1b9031
naveednadaf28 <naveednadaf@thestackly.com>
2026-06-03 16:19:53 +0530

    Gaming themes landing page first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// =========================
+// PRELOADER
+// =========================
+
+window.addEventListener("load", function () {
+
+    setTimeout(function () {
+
+        document.getElementById("preloader").style.display = "none";
+
+    }, 1500);
+
+});
+
+// =========================
+// MOBILE MENU
+// =========================
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", function () {
+
+    navLinks.classList.toggle("show");
+
+    if (navLinks.classList.contains("show")) {
+
+        menuBtn.innerHTML = "✕";
+
+    } else {
+
+        menuBtn.innerHTML = "☰";
+
+    }
+
+});
+
+// =========================
+// CLOSE MENU AFTER CLICK
+// =========================
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("show");
+
+        menuBtn.innerHTML = "☰";
+
+    });
+
+});
+
+// =========================
+// SCROLL ANIMATION
+// =========================
+
+const cards = document.querySelectorAll(
+".stat-card, .category-card, .game-card, .testimonial-card"
+);
+
+window.addEventListener("scroll", function () {
+
+    cards.forEach(card => {
+
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < window.innerHeight - 100) {
+
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+});
+
+// initial state
+
+cards.forEach(card => {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "all 0.8s ease";
+
+});
